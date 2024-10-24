5aad456b132844775cd25b48259db12a288640d7
Josue Arreola <josue@heroguest.com>
2024-10-24 00:10:02 -0600

    inital comiit

--- CordeDataTarea/ContentView.swift
+++ CordeDataTarea/ContentView.swift
@@ -4,83 +4,159 @@
 //
 //  Created by Josué Arreola on 23/10/24.
 //
-
 import SwiftUI
 import CoreData
 
 struct ContentView: View {
+    
+    @State var name: String = ""
+    @State var quantity: String = ""
     @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    @FetchRequest(entity: Product.entity(),
+               sortDescriptors: [NSSortDescriptor(key: "name", ascending: true)])
+    private var products: FetchedResults<Product>
+    
+    private func addProduct() {
+        withAnimation {
+            let product = Product(context: viewContext)
+            product.name = name
+            product.quantity = quantity
+            saveContext()
+        }
+    }
+    
+    private func deleteProducts(offsets: IndexSet) {
+        withAnimation {
+            offsets.map { products[$0] }.forEach(viewContext.delete)
+                saveContext()
+            }
+    }
+    
+    private func saveContext() {
+        do {
+            try viewContext.save()
+        } catch {
+            let error = error as NSError
+            fatalError("An error occured: \(error)")
+        }
+    }
+    
     var body: some View {
         NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+            VStack {
+                TextField(
+                    "Product name",
+                    text: $name
+                )
+                .border(Color.blue)
+                .cornerRadius(2)
+                
+                TextField(
+                    "Product quantity",
+                    text: $quantity
+                )
+                .border(Color.blue)
+                .cornerRadius(2)
+                
+                HStack {
+                    Button("Add") {
+                        addProduct()
                     }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
+                    .bold()
+                    .padding(5)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue)
+                    .foregroundStyle(.white)
+                    .cornerRadius(10)
+                    
+                    Spacer()
+                    
+                    NavigationLink(destination: ResultsView(name: name,
+                                   viewContext: viewContext))
+                    {
+                        Text("Find")
                     }
+                    .bold()
+                    .padding(5)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue)
+                    .foregroundStyle(.white)
+                    .cornerRadius(10)
+                    
+                    Spacer()
+                    
+                    Button("Clear") {
+                        name = ""
+                        quantity = ""
+                    }
+                    .bold()
+                    .padding(5)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue)
+                    .foregroundStyle(.white)
+                    .cornerRadius(10)
+                    
                 }
+                .padding(.vertical)
+                .frame(maxWidth: .infinity)
+                
+                List {
+                    ForEach(products) { product in
+                        HStack {
+                            Text(product.name ?? "Not found")
+                            Spacer()
+                            Text(product.quantity ?? "Not found")
+                        }
+                    }
+                    .onDelete(perform: deleteProducts)
+                }
+                .navigationTitle("Product Database")
+                .background(Color.white)
+                .scrollContentBackground(.hidden)
+                .listStyle(PlainListStyle())
+                
             }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+            .padding()
+            .textFieldStyle(RoundedBorderTextFieldStyle())
         }
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
+struct ResultsView: View {
+   
+   var name: String
+   var viewContext: NSManagedObjectContext
+   @State var matches: [Product]?
+
+   var body: some View {
+      
+       return VStack {
+           List {
+               ForEach(matches ?? []) { match in
+                   HStack {
+                       Text(match.name ?? "Not found")
+                       Spacer()
+                       Text(match.quantity ?? "Not found")
+                   }
+               }
+           }
+           .navigationTitle("Results")
+           .background(Color.white)
+           .scrollContentBackground(.hidden)
+           .listStyle(PlainListStyle())
+        }
+       .task {
+           let fetchRequest: NSFetchRequest<Product> = Product.fetchRequest()
+           
+           fetchRequest.entity = Product.entity()
+           fetchRequest.predicate = NSPredicate(
+               format: "name CONTAINS %@", name
+           )
+           matches = try? viewContext.fetch(fetchRequest)
+       }
+   }
+}
 
 #Preview {
-    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    ContentView()
 }
